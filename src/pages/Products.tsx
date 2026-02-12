a35ab844ea44a2b7e65ae62b4646f3696a8ba32f
Tithi Shah <tithishah26@gmail.com>
2026-02-12 16:04:45 +0530

    feat(pages): add Products page

---
+++ src/pages/Products.tsx
@@ -0,0 +1,9 @@
+import { Products as ProductsSection } from '../components/sections/Products';
+
+export const Products = () => {
+    return (
+        <div className="pt-20">
+            <ProductsSection />
+        </div>
+    );
+};
