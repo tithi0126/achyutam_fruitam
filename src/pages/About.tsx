4036aa56619bbe237e7df575ba07ea09d320dfb1
Tithi Shah <tithishah26@gmail.com>
2026-02-12 16:04:39 +0530

    feat(pages): add About page

---
+++ src/pages/About.tsx
@@ -0,0 +1,9 @@
+import { About as AboutSection } from '../components/sections/About';
+
+export const About = () => {
+    return (
+        <div className="pt-20">
+            <AboutSection />
+        </div>
+    );
+};
