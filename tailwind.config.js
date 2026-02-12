a761489c3ecfd57aeac883831f0df86e45513d82
Tithi Shah <tithishah26@gmail.com>
2026-02-12 16:03:17 +0530

    chore: project skeleton and configuration

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                brand: {
+                    green: '#0D5C5C',
+                    dark: '#0A4848',
+                    light: '#E6F2F2',
+                },
+                fruit: {
+                    mango: '#FF9F1C',
+                    pomegranate: '#E63946',
+                    guava: '#F77F00',
+                }
+            },
+            fontFamily: {
+                heading: ['Josefin Sans', 'sans-serif'],
+                body: ['Inter', 'sans-serif'],
+            }
+        },
+    },
+    plugins: [],
+}
